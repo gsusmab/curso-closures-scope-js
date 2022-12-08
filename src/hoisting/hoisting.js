1e1bdd070dbf3484b6cfdb78f5a076d9021da04d
Jesus Almaraz <gsusmab@gmail.com>
2022-12-08 16:35:08 -0600

    Interactuando con el Hoisting en JS

---
+++ src/hoisting/hoisting.js
@@ -0,0 +1,13 @@
+// var nameOfDog; Undefined
+console.log(nameOfDog);
+var nameOfDog = "Tommy";
+console.log(nameOfDog);
+
+//var name; Undefined
+greeting();
+
+function greeting() {
+  console.log(`Hola, ${name} como estas?`);
+}
+
+var name = "jesus";
